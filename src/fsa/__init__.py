c481b88245505363d9db9fa34c2794f5d3e6615f
Martin Poliak <martin.poliak@keenswh.com>
2017-01-24 17:39:42 +0100

    Finished the generator. Needs code cleanup.

--- src/fsa/__init__.py
+++ src/fsa/__init__.py
@@ -20,8 +20,26 @@ class MiniTasksAutomatonInterface():
         self.positive_table = self._parse_positive_from_description()
         self.negative_table = self._parse_negative_from_description()
         self.anything_allowed = self._parse_anything_from_description()
-        self.positive_automaton, self.positive_state_depth_dict = self._build_automaton(self.positive_table)
-        self.negative_automaton, self.negative_state_depth_dict = self._build_automaton(self.negative_table)
+        assert((len(self.negative_table) == 0) or self.anything_allowed)  # "not" occurs => self.anything_allowed
+        self.positive_automaton, self.positive_state_depth_dict, self.positive_alphabet = self._build_automaton(self.positive_table)
+        self.negative_automaton, self.negative_state_depth_dict, self.negative_alphabet = self._build_automaton(self.negative_table)
+        self._distribute_remaining_alphabet_symbols()
+
+    def _distribute_remaining_alphabet_symbols(self):
+        # takes unused alphabet symbols and distributes them between the positive and negative alphabets
+        if len(self.negative_alphabet) == 0:
+            self.positive_alphabet = list(self._sigma)
+            return
+
+        all = set(self._sigma)
+        all.difference_update(set(self.positive_alphabet))
+        all.difference_update(self.negative_alphabet)
+        for symbol in all:
+            if random.random() > 0.5:
+                self.positive_alphabet.append(symbol)
+            else:
+                self.negative_alphabet.append(symbol)
+        return
 
     def _is_in_final_state(self, automaton, state):
         for f in automaton.Final:
@@ -38,6 +56,7 @@ class MiniTasksAutomatonInterface():
         nfa.addState(initial_state)
         nfa.addInitial(initial_state)
         state = 1
+        alphabet = []
 
         for string in ngram_table:
             source_state = initial_state
@@ -46,6 +65,7 @@ class MiniTasksAutomatonInterface():
                 # loop on initial state is added during usage of the automaton to increase efficiency
                 nfa.addState(state)
                 nfa.addTransition(source_state, string[i], state)
+                alphabet.append(string[i])
                 state_depth_dict[state] = i + 1
                 if i == len(string) - 1:
                     nfa.addFinal(state)
@@ -53,7 +73,7 @@ class MiniTasksAutomatonInterface():
                 source_state = state
                 state += 1
 
-        return nfa, state_depth_dict # final_state_ngram_length dictionary will be useful for accepting
+        return nfa, state_depth_dict, alphabet # final_state_ngram_length dictionary will be useful for accepting
 
     def _parse_positive_from_description(self):
         ret = []
@@ -115,7 +135,54 @@ class MiniTasksAutomatonInterface():
 
         return symbols
 
+    # the implementation of this function could be improved, it's a baseline implementation
+    def _get_correct_not_string(self, length, current_chrs):
+        chrs = []
+        automaton = self.negative_automaton
+        state = automaton.epsilonClosure(automaton.Initial)
+        initial = state.copy()
+
+        # to avoid a bug that it puts two almost not strings together to form a full not string
+        for c in current_chrs:
+            state = automaton.evalSymbol(state, c).union(initial)
+
+        alphabet_to_use = self.negative_alphabet
+
+        for pos in range(length):
+            random.shuffle(alphabet_to_use)
+            for symbol in alphabet_to_use:
+                try_state = automaton.evalSymbol(state, symbol)
+                if not self._is_in_final_state(automaton, try_state):
+                    state = try_state.union(initial)  # loop to be a matcher automaton
+                    chrs.append(symbol)
+                    break
+
+        return chrs
+
+    # the implementation of this function could be improved, it's a baseline implementation
+    def _get_correct_anything_string(self, length, state):
+        chrs = []
+        automaton = self.positive_automaton
+        initial = state.copy()
+        state = state.copy()
+
+        alphabet_to_use = self.positive_alphabet
+
+        for pos in range(length):
+            random.shuffle(alphabet_to_use)
+            for symbol in alphabet_to_use:
+                try_state = automaton.evalSymbol(state, symbol)
+                if not self._is_in_final_state(automaton, try_state):
+                    state = try_state.union(initial)  # loop to be a matcher automaton
+                    chrs.append(symbol)
+                    break
+
+        return chrs, state
+
     def get_correct_string(self):
+        return self._get_string(True)
+
+    def _get_string(self, make_correct = True, avoid_states = None, insert_not_string = None):
         # positive automaton part first
         current = last_confirmed = -1
         automaton = self.positive_automaton
@@ -127,51 +194,105 @@ class MiniTasksAutomatonInterface():
         attractor_state = None
 
         while True:
-            symbols = self._get_available_transition_symbols(automaton, self.positive_state_depth_dict, state,
-                                                             current - last_confirmed)
-            next_symbol = None
-            pick_next_symbol_randomly = True
+            if last_confirmed == current and \
+                    self.anything_allowed:
+                # (last_confirmed == current) is true after a final state is entered
+                # when "not" strings are used, there's always "anything" allowed (asserted above)
+                if random.random() > 0.5:
+                    # random: do not insert the "anything" strings always
+                    if random.random() > 0.5 or len(self.negative_table) == 0:
+                        newchrs, state = self._get_correct_anything_string(int(math.ceil(random.random()*5)), state)
+                    else:
+                        newchrs = self._get_correct_not_string(int(math.ceil(random.random()*5)), chrs)
+                        state = initial.copy()
+                    current = last_confirmed = current + len(newchrs)
+                    chrs.extend(newchrs)
 
-            if current >= threshold_length:
+            if len(self.positive_table) > 0:
+                symbols = self._get_available_transition_symbols(automaton, self.positive_state_depth_dict, state,
+                                                                 current - last_confirmed)
 
-                if len(missing_states) > 0 and self.logical_op == self._and_string:
-                    if attractor_state not in missing_states:
-                        attractor_state = random.sample(missing_states, 1)[0]
-                    attractor_state_depth = self.positive_state_depth_dict[attractor_state]
-                    to_try = list(range(attractor_state - attractor_state_depth+1, attractor_state+1))
-                    to_try.reverse()
+                next_symbol = None
+                pick_next_symbol_randomly = True
 
-                    # try to reach missing states when in "and" mode
-                    # find a symbol that will lead to one of the missing states (if there is such a symbol)
-                    for possible_target in to_try:
-                        did_break = False
-                        for symbol in symbols:
-                            target = automaton.evalSymbol(state, symbol)
-                            if possible_target in target:
-                                state = target
-                                next_symbol = symbol
-                                pick_next_symbol_randomly = False
-                                did_break = True
+                if current >= threshold_length:
+                    if make_correct:
+                        if len(missing_states) > 0 and self.logical_op == self._and_string:
+                            if attractor_state not in missing_states:
+                                attractor_state = random.sample(missing_states, 1)[0]
+                            attractor_state_depth = self.positive_state_depth_dict[attractor_state]
+                            to_try = list(range(attractor_state - attractor_state_depth+1, attractor_state+1))
+                            to_try.reverse()
+
+                            # try to reach missing states when in "and" mode
+                            # find a symbol that will lead to one of the missing states (if there is such a symbol)
+                            for possible_target in to_try:
+                                did_break = False
+                                for symbol in symbols:
+                                    target = automaton.evalSymbol(state, symbol)
+                                    if possible_target in target:
+                                        state = target
+                                        next_symbol = symbol
+                                        pick_next_symbol_randomly = False
+                                        did_break = True
+                                        break
+                                if did_break:
+                                    break
+
+                        else:
+                            if self._is_in_final_state(automaton,state):
                                 break
-                        if did_break:
-                            break
-
-                else:
-                    if self._is_in_final_state(automaton,state):
+                    else:
                         break
 
-            if pick_next_symbol_randomly:
-                next_symbol = random.sample(symbols, 1)[0]
-                state = automaton.evalSymbol(state, next_symbol).union(initial)
+                current += 1
 
-            missing_states = missing_states.difference(state)
-            current+=1
-            chrs.append(next_symbol)
-            if self._is_in_final_state(automaton,state):
-                last_confirmed = current
+                if pick_next_symbol_randomly:
+                    if make_correct or len(avoid_states) == 0:
+                        next_symbol = random.sample(symbols, 1)[0]
+                        state = automaton.evalSymbol(state, next_symbol).union(initial)
+                    else:  # generate a symbol that will not lead to one of the states in avoid_states
+                        found_symbol = False
+                        symbol_list = list(symbols)
+                        random.shuffle(symbol_list)
+                        for symbol in symbol_list:
+                            try_state = automaton.evalSymbol(state, symbol)
+                            if len(try_state.intersection(avoid_states)) == 0:
+                                next_symbol = symbol
+                                found_symbol = True
+                            else:
+                                last_confirmed = current  # we're avoiding a final state: pretend there was a match
+                        if not found_symbol or random.random() > 0.9:
+                            next_symbol = random.sample(set(self._sigma).difference(symbols),1)[0]
+                            last_confirmed = current
+                        prev_state = state
+                        state = automaton.evalSymbol(state, next_symbol).union(initial)
+                        #if(self._is_in_avoid_state(automaton,state,avoid_states)):
+                        #    assert(False)
+
+                missing_states = missing_states.difference(state)
+
+                chrs.append(next_symbol)
+                if self._is_in_final_state(automaton,state):
+                    last_confirmed = current
+            else:
+                if current >= threshold_length:
+                    break
+
+        if not make_correct and insert_not_string is not None:
+            randSplit = int(math.floor(random.random()*len(chrs)))
+            chrsA = chrs[:randSplit]
+            chrsB = chrs[randSplit:]
+            chrsA.extend(list(insert_not_string))
+            chrsA.extend(chrsB)
+            chrs = chrsA
 
         str = ''.join(chrs)
-        assert(self.is_string_correct(str))
+        if not self.is_string_correct(str) == make_correct:
+            print(str)
+            print(self.description)
+            print(self.logical_op)
+        assert(self.is_string_correct(str) == make_correct)
         return str
 
     def _get_random_string(self, length):
@@ -179,16 +300,44 @@ class MiniTasksAutomatonInterface():
         return "".join(chrlist)
 
     def _get_random_wrong_string(self):
-        for i in range(100):
+        for i in range(3):
             string_length = int(math.ceil(random.random()*20))
             str = self._get_random_string(string_length)
             if not self.is_string_correct(str):
                 return str
-        raise AssertionError("could not generate a random string that would not be accepted by the automaton")
+        return None  # raise AssertionError("could not generate a random string that is not accepted by the automaton")
+
+    def _get_almost_correct_string(self):
+        # kinds of almost correct strings:
+        # 1 + 2: or case: choose all final states and don't allow entry into those states
+        # 3: and case: choose one final state and don't allow entry into that state
+        # 4: not case: either take the strategy from above or insert a bad not string (50/50)
+
+        if len(self.negative_table) == 0 and len(self.positive_automaton.Final) == 0:
+            return ""  # can happen if there's just "anything" as the language description
+
+        avoid_states = set()
+        insert_not_string = None
+
+        if self.logical_op == self._or_string:
+            avoid_states = self.positive_automaton.Final
+        elif len(self.negative_table) == 0:
+            avoid_states = random.sample(self.positive_automaton.Final,1)
+        else:
+            if random.random() > 0.5 and len(self.positive_automaton.Final) > 0:
+                avoid_states = random.sample(self.positive_automaton.Final,1)
+            else:
+                insert_not_string = random.choice(self.negative_table)
+
+        return self._get_string(False,avoid_states,insert_not_string)
 
     def get_wrong_string(self, randomization=1.0):
-        str = self._get_random_wrong_string()
-        return str
+        string = None
+        if(randomization == 1.0):
+            string = self._get_random_wrong_string()
+        if(string == None):
+            string = self._get_almost_correct_string()
+        return string
 
     def _eval_positive(self, string):
         """Verify if the positive NFA recognises given word.
--- src/fsa/tests/test_fsa.py
+++ src/fsa/tests/test_fsa.py
@@ -4,6 +4,11 @@ from src.fsa import build_automaton
 
 class TestFSA(unittest.TestCase):
 
+    def assertGeneratedStrings(self, obj):
+        for i in range(1000):
+            self.assertTrue(obj.is_string_correct(obj.get_correct_string()))
+            self.assertFalse(obj.is_string_correct(obj.get_wrong_string(0)))
+
     def test_fsa_1_1(self):
         obj = build_automaton("C", "and")
         self.assertTrue(obj.is_string_correct("C"))
@@ -12,6 +17,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("CCY"))
         self.assertFalse(obj.is_string_correct("ABGD"))
         self.assertFalse(obj.is_string_correct(""))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_1_2(self):
         obj = build_automaton("C", "or")
@@ -21,6 +27,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("CCY"))
         self.assertFalse(obj.is_string_correct("ABGD"))
         self.assertTrue(obj.is_string_correct(""))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_1_3(self):
         obj = build_automaton("AB", "and")
@@ -28,6 +35,7 @@ class TestFSA(unittest.TestCase):
         self.assertTrue(obj.is_string_correct("ABABAB"))
         self.assertFalse(obj.is_string_correct("ABABABAA"))
         self.assertFalse(obj.is_string_correct("AXBGD"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_1_4(self):
         obj = build_automaton("FJG", "and")
@@ -37,6 +45,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("FFF"))
         self.assertFalse(obj.is_string_correct("EFGTR"))
         self.assertFalse(obj.is_string_correct(""))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_1_5(self):
         obj = build_automaton("XYX", "or")
@@ -46,6 +55,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("XYXY"))
         self.assertFalse(obj.is_string_correct("OUAJHFLJAH"))
         self.assertTrue(obj.is_string_correct(""))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_1_6(self):
         obj = build_automaton("XX", "and")
@@ -53,6 +63,7 @@ class TestFSA(unittest.TestCase):
         self.assertTrue(obj.is_string_correct("XXXX"))
         self.assertFalse(obj.is_string_correct("XXY"))
         self.assertFalse(obj.is_string_correct("ABGD"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_2_1(self):
         obj = build_automaton("anything", "or")
@@ -62,6 +73,7 @@ class TestFSA(unittest.TestCase):
         self.assertTrue(obj.is_string_correct("XYYYYYABCYXXXYXYXYYYYABC"))
         self.assertTrue(obj.is_string_correct("XXYZABC"))
         self.assertTrue(obj.is_string_correct("ABCGDTRW"))
+        # self.assertGeneratedStrings(obj)  # cannot generate a wrong string in this case
 
     def test_fsa_2_2(self):
         obj = build_automaton("AB CD", "or")
@@ -70,6 +82,7 @@ class TestFSA(unittest.TestCase):
         self.assertTrue(obj.is_string_correct("ABABAB"))
         self.assertFalse(obj.is_string_correct("CDA"))
         self.assertFalse(obj.is_string_correct("ABGDTRW"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_2_3(self):
         obj = build_automaton("FAB GG MIL", "or")
@@ -80,6 +93,7 @@ class TestFSA(unittest.TestCase):
         self.assertTrue(obj.is_string_correct("FABGGGMIL"))
         self.assertFalse(obj.is_string_correct("FABGGGMILFA"))
         self.assertFalse(obj.is_string_correct("ABCGDTRW"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_2_4(self):
         obj = build_automaton("X Y", "or")
@@ -88,6 +102,7 @@ class TestFSA(unittest.TestCase):
         self.assertTrue(obj.is_string_correct("XYYYYYYXXXYXYXYYYY"))
         self.assertFalse(obj.is_string_correct("XXYZ"))
         self.assertFalse(obj.is_string_correct("ABGDTRW"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_2_5(self):
         obj = build_automaton("X Y ABC", "or")
@@ -97,6 +112,7 @@ class TestFSA(unittest.TestCase):
         self.assertTrue(obj.is_string_correct("XYYYYYABCYXXXYXYXYYYYABC"))
         self.assertFalse(obj.is_string_correct("XXYZABC"))
         self.assertFalse(obj.is_string_correct("ABCGDTRW"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_2_6(self):
         obj = build_automaton("C CAB ABC", "or")
@@ -106,6 +122,7 @@ class TestFSA(unittest.TestCase):
         self.assertTrue(obj.is_string_correct("CABCABCCCC"))
         self.assertFalse(obj.is_string_correct("ABCABCA"))
         self.assertFalse(obj.is_string_correct("ABCGDTRW"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_3_1(self):
         obj = build_automaton("AB CF", "and")
@@ -117,6 +134,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("AB"))
         self.assertFalse(obj.is_string_correct("ABCAFAB"))
         self.assertFalse(obj.is_string_correct("AOPYQEG"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_3_2(self):
         obj = build_automaton("HL RM BT", "and")
@@ -128,6 +146,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("HL"))
         self.assertFalse(obj.is_string_correct("HLRMBBT"))
         self.assertFalse(obj.is_string_correct("AOPYQEG"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_3_3(self):
         obj = build_automaton("GLE EA ABC", "and")
@@ -138,6 +157,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("GLEABCA"))
         self.assertFalse(obj.is_string_correct("ABCABCABCGLE"))
         self.assertFalse(obj.is_string_correct("AOPYQEG"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_3_4(self):
         obj = build_automaton("AB anything", "and")
@@ -149,6 +169,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("GLEEA"))
         self.assertFalse(obj.is_string_correct("GLEAACA"))
         self.assertFalse(obj.is_string_correct("AOPYQEG"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_3_5(self):
         obj = build_automaton("AB CF anything", "and")
@@ -160,6 +181,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("GLEEAB"))
         self.assertFalse(obj.is_string_correct("GLEAACFA"))
         self.assertFalse(obj.is_string_correct("AOPYQEG"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_4_1(self):
         obj = build_automaton("not AB anything", "and")
@@ -172,6 +194,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("GLEEAB"))
         self.assertFalse(obj.is_string_correct("GLEABACFA"))
         self.assertFalse(obj.is_string_correct("ABOPYQEG"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_4_2(self):
         obj = build_automaton("not AB CF anything", "and")
@@ -184,6 +207,7 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("GLEEABCF"))
         self.assertFalse(obj.is_string_correct("GLEABACFA"))
         self.assertFalse(obj.is_string_correct("ABOPYQEG"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_4_3(self):
         obj = build_automaton("not AB not CF anything", "and")
@@ -197,19 +221,34 @@ class TestFSA(unittest.TestCase):
         self.assertFalse(obj.is_string_correct("GLEABACA"))
         self.assertFalse(obj.is_string_correct("GLEABACFA"))
         self.assertFalse(obj.is_string_correct("ABOPYQEG"))
+        self.assertGeneratedStrings(obj)
 
     def test_fsa_generator_1(self):
         obj = build_automaton("C", "and")
-        self.assertFalse(obj.is_string_correct(obj.get_wrong_string(1)))
+        for ind in range(100):
+            self.assertFalse(obj.is_string_correct(obj.get_wrong_string(1)))
 
     def test_fsa_generator_2(self):
         obj = build_automaton("AB CF ABC", "or")
         for ind in range(100):
-            str = obj.get_correct_string()
-            self.assertTrue(obj.is_string_correct(str))
+            string = obj.get_correct_string()
+            self.assertTrue(obj.is_string_correct(string))
 
     def test_fsa_generator_3(self):
         obj = build_automaton("AB CF ABC", "and")
         for ind in range(100):
-            str = obj.get_correct_string()
-            self.assertTrue(obj.is_string_correct(str))
+            string = obj.get_correct_string()
+            self.assertTrue(obj.is_string_correct(string))
+
+    def test_fsa_generator_4(self):
+        obj = build_automaton("MNO KL not CF not ABC anything", "and")
+        for ind in range(100):
+            string = obj.get_correct_string()
+            self.assertTrue(obj.is_string_correct(string))
+
+# obj = build_automaton("AB anything", "or")  # this is not handled
+
+# obj = build_automaton("AB CF", "and")
+# for ind in range(1000):
+#     string = obj.get_wrong_string(0)
+#     assert(not obj.is_string_correct(string))
