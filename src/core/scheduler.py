e5d2e1d1ab48ed77a2fd6507f3cbbfa5bb922a7d
Martin Stránský <martin.stransky@keenswh.com>
2017-01-26 13:59:29 +0100

    scheduler fix

--- src/core/scheduler.py
+++ src/core/scheduler.py
@@ -63,7 +63,7 @@ class IncrementalTaskScheduler:
         self.success_threshold = success_threshold
 
     def get_next_task(self):
-        if self.reward_count == self.success_threshold - 1:
+        if self.reward_count >= self.success_threshold:
             self.reward_count = 0
             self.task_ptr = (self.task_ptr + 1) % len(self.tasks)
         return self.tasks[self.task_ptr]
@@ -88,7 +88,7 @@ class ConsecutiveTaskScheduler:
         self.success_threshold = success_threshold
 
     def get_next_task(self):
-        if self.reward_count == self.success_threshold - 1:
+        if self.reward_count >= self.success_threshold:
             self.reward_count = 0
             self.task_ptr = (self.task_ptr + 1) % len(self.tasks)
         return self.tasks[self.task_ptr]
