6c2edc5ba07449e82248a68c320ac8b6c75a9194
Michal Vlasák <michal.vlasak@keenswh.com>
2017-01-30 15:35:51 +0100

    Duplicit TaskSet1 removed, small refactoring

--- src/tasks/good_ai/comm_ai_mini.py
+++ src/tasks/good_ai/comm_ai_mini.py
@@ -73,74 +73,72 @@ class TaskSet0(BaseTask):
         self.set_reward(0, self.give_away_message)
 
 
-class TaskSet1(BaseTask):
-    def __init__(self, world=None):
-        super(TaskSet1, self).__init__(world=world, max_time=3000)
-
-# generovat incorrect z distribuce correct
-# pridat minimalni delku
-def get_task(max_length_of_description, max_nr_of_groups, max_length_of_verify, description_type,
-             not_portion=None, subset_size=None, without_anything=None):
-    not_portion = not_portion or 0
-    subset_size = subset_size or 13
-
-    if not description_type == "and" and not description_type == "or":
-        print("Unknown description_type: {}".format(description_type))
-        return
-    if subset_size > 13:
-        print("Subset size cannot be higher than 13.")
-        return
-
-    nr_of_groups = random.randint(1, max_nr_of_groups)
-    if not_portion > 0:
-        alphabet = string.ascii_uppercase
-        normal_subset = "".join(random.choice(alphabet) for _ in range(subset_size))
-        reduced_alphabet = "".join([char for char in alphabet if char not in normal_subset])
-        not_subset = "".join(random.choice(reduced_alphabet) for _ in range(subset_size))
-
-    descriptions = []
-    if without_anything:
-        has_anything = False
-    else:
-        has_anything = random.choice([True, False])
-
-    if description_type == "or" and not without_anything:
-        if random.random() < 0.05:
-            has_anything = True
-            nr_of_groups = 0
-        else:
-            has_anything = False
-
-    for _ in range(nr_of_groups):
-        length_of_description = random.randint(1, max_length_of_description)
-        if not_portion > 0:
-            if random.random() < not_portion:
-                description = "not " + random_string_from(length_of_description, not_subset)
-                has_anything = True
-            else:
-                description = random_string_from(length_of_description, normal_subset)
-        else:
-            description = random_string(length_of_description)
-        descriptions.append(description)
-
-    if has_anything:
-        descriptions.append("anything")
-
-    descriptions = list(set(descriptions))
-    automaton = build_automaton(" ".join(descriptions), description_type)
-    type_connection = " {} ".format(description_type)
-    complete_description = type_connection.join(descriptions)
-    is_correct = random.choice([True, False])
-
-    verify_length = random.randint(1, max_length_of_verify)
-    if is_correct:
-        verify = automaton.get_correct_string(verify_length)
-    else:
-        verify = automaton.get_wrong_string(verify_length, 0)
-    return (is_correct, "description: {}; verify {}".format(complete_description, verify))
-
-
 class TaskSetBase(BaseTask):
+
+    @staticmethod
+    def get_task(max_length_of_description, max_nr_of_groups, max_length_of_verify, description_type,
+                 not_portion=None, subset_size=None, without_anything=None):
+        '''
+        TODO: generovat incorrect z distribuce correct
+        TODO: pridat minimalni delku
+        '''
+        not_portion = not_portion or 0
+        subset_size = subset_size or 13
+
+        if not description_type == "and" and not description_type == "or":
+            print("Unknown description_type: {}".format(description_type))
+            return
+        if subset_size > 13:
+            print("Subset size cannot be higher than 13.")
+            return
+
+        nr_of_groups = random.randint(1, max_nr_of_groups)
+        if not_portion > 0:
+            alphabet = string.ascii_uppercase
+            normal_subset = "".join(random.choice(alphabet) for _ in range(subset_size))
+            reduced_alphabet = "".join([char for char in alphabet if char not in normal_subset])
+            not_subset = "".join(random.choice(reduced_alphabet) for _ in range(subset_size))
+
+        descriptions = []
+        if without_anything:
+            has_anything = False
+        else:
+            has_anything = random.choice([True, False])
+
+        if description_type == "or" and not without_anything:
+            if random.random() < 0.05:
+                has_anything = True
+                nr_of_groups = 0
+            else:
+                has_anything = False
+
+        for _ in range(nr_of_groups):
+            length_of_description = random.randint(1, max_length_of_description)
+            if not_portion > 0:
+                if random.random() < not_portion:
+                    description = "not " + random_string_from(length_of_description, not_subset)
+                    has_anything = True
+                else:
+                    description = random_string_from(length_of_description, normal_subset)
+            else:
+                description = random_string(length_of_description)
+            descriptions.append(description)
+
+        if has_anything:
+            descriptions.append("anything")
+
+        descriptions = list(set(descriptions))
+        automaton = build_automaton(" ".join(descriptions), description_type)
+        type_connection = " {} ".format(description_type)
+        complete_description = type_connection.join(descriptions)
+        is_correct = random.choice([True, False])
+
+        verify_length = random.randint(1, max_length_of_verify)
+        if is_correct:
+            verify = automaton.get_correct_string(verify_length)
+        else:
+            verify = automaton.get_wrong_string(verify_length, 0)
+        return (is_correct, "description: {}; verify {}".format(complete_description, verify))
 
     def __init__(self, world=None):
         super(TaskSetBase, self).__init__(world=world, max_time=3000)
@@ -157,8 +155,8 @@ class TaskSetBase(BaseTask):
         if not self.max_length_of_description or not self.max_nr_of_groups or not self.max_length_of_verify or not self.description_type:
             raise AttributeError("Some of the TaskSet attributes are not set!")
 
-        is_correct, task = get_task(self.max_length_of_description, self.max_nr_of_groups, self.max_length_of_verify,
-                                    self.description_type, self.not_portion, self.subset_size, self.without_anything)
+        is_correct, task = TaskSetBase.get_task(self.max_length_of_description, self.max_nr_of_groups, self.max_length_of_verify,
+                                                self.description_type, self.not_portion, self.subset_size, self.without_anything)
 
         self.answer = "true" if is_correct else "false"
         self.give_away_message = 'Wrong. The right answer is: {}.'.format(self.answer)
