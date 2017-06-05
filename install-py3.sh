c9a6e7df64f41a63a4b1d27da77bbed30bfff4f7
Ubuntu <premil@ftest.4n3onhx1mq3ufhv3a1dlwjo0fh.xx.internal.cloudapp.net>
2017-06-05 16:01:42 +0000

    Add install and run scripts for Python 3.

---
+++ install-py3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install python3-pip
+
+pip3 install -r src/requirements/py3.txt
---
+++ run-remote.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 src/run.py src/tasks_config.challenge.json -l learners.base.RemoteLearner
