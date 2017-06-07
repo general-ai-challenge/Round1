af05f66e4a8bc20330eff438c475040578edba03
Honza Šinkora <jan.sinkora@gmail.com>
2017-06-07 17:38:37 +0200

    Fixed typos

--- evaluation/examples/linux-cpp/build.sh
+++ evaluation/examples/linux-cpp/build.sh
@@ -8,6 +8,6 @@ cd ..
 
 docker build -t learner . || exit 1
 
-echo "Saving and compressing the image, it make take a while..."
+echo "Saving and compressing the image, it may take a while..."
 docker save learner | gzip -c > learner.tar.gz
 
--- evaluation/examples/linux-python/build.sh
+++ evaluation/examples/linux-python/build.sh
@@ -2,6 +2,6 @@
 
 docker build -t learner . || exit 1
 
-echo "Saving and compressing the image, it make take a while..."
+echo "Saving and compressing the image, it may take a while..."
 docker save learner | gzip -c > learner.tar.gz
 
