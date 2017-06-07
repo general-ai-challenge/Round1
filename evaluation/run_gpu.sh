ad4248e8556924096b012f37586e5b4afe1f8014
Ubuntu <premil@utest.4n3onhx1mq3ufhv3a1dlwjo0fh.xx.internal.cloudapp.net>
2017-06-07 14:41:14 +0000

    Run scripts: add x flag, improve Ux.

--- evaluation/run_gpu.sh
+++ evaluation/run_gpu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-gunzip -c learner.tar.gz | docker load
+echo "Loading image, it may take a while..."
+gunzip -c learner.tar.gz | docker load || exit 1
+
+echo "Running the image..."
 nvidia-docker run -d --user 1000:1000 --network=challengenetwork learner
 
