b45b61e45b3af6408dae1c2f02c9ee9b95f85af1
Stephen B. Hope <stephenbhope@gmail.com>
2017-06-09 02:37:25 -0400

    clean

--- src/core/tests/test_serializer.py
+++ src/core/tests/test_serializer.py
@@ -5,12 +5,8 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+
 import unittest
-import sys
 from core import serializer
 
 
@@ -25,22 +21,14 @@ class TestSerializer(unittest.TestCase):
         self.assertEqual(u"\u03B1", slzr.to_text(slzr.to_binary(u"\u03B1")))
 
     def testScramblingSerializerWrapper(self):
-        slzr = serializer.ScramblingSerializerWrapper(
-            serializer.StandardSerializer())
-        self.assertEqual(slzr.tokenize("a b"),
-                         [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD')])
-        self.assertEqual(slzr.tokenize("a  b"),
-                         [("a", "WORD"), (" ", 'SILENCE'), (" ", 'SILENCE'),
-                          ('b', 'WORD')])
-        self.assertEqual(slzr.tokenize("a b "),
-                         [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'),
-                          (' ', 'SILENCE')])
-        self.assertEqual(slzr.tokenize("a b, "),
-                         [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'),
-                          (',', 'PUNCT'), (' ', 'SILENCE')])
-        self.assertEqual(slzr.tokenize("a b ."),
-                         [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'),
-                          (' ', 'SILENCE'), ('.', 'PUNCT')])
+        slzr = serializer.ScramblingSerializerWrapper(serializer.StandardSerializer())
+        self.assertEqual(slzr.tokenize("a b"), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD')])
+        self.assertEqual(slzr.tokenize("a  b"), [("a", "WORD"), (" ", 'SILENCE'), (" ", 'SILENCE'), ('b', 'WORD')])
+        self.assertEqual(slzr.tokenize("a b "), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'), (' ', 'SILENCE')])
+        self.assertEqual(slzr.tokenize(
+            "a b, "), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'), (',', 'PUNCT'), (' ', 'SILENCE')])
+        self.assertEqual(slzr.tokenize(
+            "a b ."), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'),(' ', 'SILENCE'), ('.', 'PUNCT')])
 
 
 def main():
