ed1229bff1466985ef388efef4f0909e8de87217
Stephen B. Hope <stephenbhope@gmail.com>
2017-06-10 03:03:43 -0400

    clean

--- src/core/tests/test_serializer.py
+++ src/core/tests/test_serializer.py
@@ -11,24 +11,32 @@ from core import serializer
 
 
 class TestSerializer(unittest.TestCase):
+    """
 
+    """
     def testConsistency(self):
+        """ greek letter \alpha (not working in current ascii serialization)
+
+        :return:
+        """
         slzr = serializer.StandardSerializer()
         self.assertEqual('a', slzr.to_text(slzr.to_binary('a')))
         self.assertEqual(' ', slzr.to_text(slzr.to_binary(' ')))
-
-        # greek letter \alpha (not working in current ascii serialization)
         self.assertEqual(u"\u03B1", slzr.to_text(slzr.to_binary(u"\u03B1")))
 
     def testScramblingSerializerWrapper(self):
+        """
+
+        :return:
+        """
         slzr = serializer.ScramblingSerializerWrapper(serializer.StandardSerializer())
         self.assertEqual(slzr.tokenize("a b"), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD')])
         self.assertEqual(slzr.tokenize("a  b"), [("a", "WORD"), (" ", 'SILENCE'), (" ", 'SILENCE'), ('b', 'WORD')])
         self.assertEqual(slzr.tokenize("a b "), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'), (' ', 'SILENCE')])
-        self.assertEqual(slzr.tokenize(
-            "a b, "), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'), (',', 'PUNCT'), (' ', 'SILENCE')])
-        self.assertEqual(slzr.tokenize(
-            "a b ."), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'),(' ', 'SILENCE'), ('.', 'PUNCT')])
+        self.assertEqual(slzr.tokenize("a b, "), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'), (
+            ',', 'PUNCT'), (' ', 'SILENCE')])
+        self.assertEqual(slzr.tokenize("a b ."), [("a", "WORD"), (" ", 'SILENCE'), ('b', 'WORD'),(
+            ' ', 'SILENCE'), ('.', 'PUNCT')])
 
 
 def main():
