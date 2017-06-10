319be682318970014704d9491e23e831610ae0c3
Stephen B. Hope <stephenbhope@gmail.com>
2017-06-10 07:45:54 -0400

    add header

--- src/core/channels.py
+++ src/core/channels.py
@@ -1,9 +1,11 @@
-# Copyright (c) 2016-present, Facebook, Inc.
-# All rights reserved.
+# -*- coding: utf-8
 #
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# (c) 2017, Stephen B, Hope,  All rights reserved.
+#
+# CommAI-env Copyright (c) 2016-present, Facebook, Inc., All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this
+# source tree. An additional grant of patent rights can be found in the PATENTS file in the same directory.
 
 from core.obs.observer import Observable
 import logging
