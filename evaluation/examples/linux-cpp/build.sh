c463af47f45763088627c722e987e61ac76775d4
Stephen B. Hope <stephenbhope@gmail.com>
2017-06-10 08:31:49 -0400

    add header

--- evaluation/examples/linux-cpp/build.sh
+++ evaluation/examples/linux-cpp/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# -*- coding: utf-8
+#
+# Copyright (c) 2017, Stephen B, Hope,  All rights reserved.
+#
+# CommAI-env Copyright (c) 2016-present, Facebook, Inc., All rights reserved.
+# Round1 Copyright (c) 2017-present, GoodAI All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE_CHALLENGE file in the root directory of this source tree.
 
 cd agent
 
