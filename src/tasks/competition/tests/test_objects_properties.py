7948507e96a546d498e7679cdf68df375d3e0071
Stephen B. Hope <stephenbhope@gmail.com>
2017-06-10 08:36:51 -0400

    add header

--- src/tasks/competition/tests/test_objects_properties.py
+++ src/tasks/competition/tests/test_objects_properties.py
@@ -1,14 +1,13 @@
-# Copyright (c) 2016-present, Facebook, Inc.
-# All rights reserved.
+# -*- coding: utf-8
+#
+# Copyright (c) 2017, Stephen B, Hope,  All rights reserved.
+#
+# CommAI-env Copyright (c) 2016-present, Facebook, Inc., All rights reserved.
+# Round1 Copyright (c) 2017-present, GoodAI All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# LICENSE_CHALLENGE file in the root directory of this source tree.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 import unittest
 import tasks.competition.messages as msg
 import tasks.competition.objects_properties as objects_properties
