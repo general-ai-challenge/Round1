d047a507da76bd2a26514278a1b592190e28553d
Stephen B. Hope <stephenbhope@gmail.com>
2017-06-10 03:10:08 -0400

    clean

--- src/core/tests/test_task.py
+++ src/core/tests/test_task.py
@@ -10,26 +10,61 @@ import core.task as task
 
 
 class TestEvents(unittest.TestCase):
+    """
+
+    """
 
     def testTriggers(self):
+        """
+
+        :return:
+        """
         class TestTask(task.Task):
+            """
+
+            """
             def __init__(self, *args, **kwargs):
+                """
+
+                :param args:
+                :param kwargs:
+                """
                 super(TestTask, self).__init__(*args, **kwargs)
 
             @task.on_start()
             def start_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
             @task.on_message()
             def message_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
             @task.on_timeout()
             def timeout_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
             @task.on_ended()
             def ended_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
         tt = TestTask(max_time=10)
@@ -42,28 +77,56 @@ class TestEvents(unittest.TestCase):
         self.assertIn(self.get_func(TestTask.ended_handler), handlers)
         types = dict((t.event_handler, t.type) for t in triggers)
         self.assertEqual(task.Start, types[self.get_func(TestTask.start_handler)])
-        self.assertEqual(task.MessageReceived,
-                         types[self.get_func(TestTask.message_handler)])
-        self.assertEqual(task.Timeout,
-                         types[self.get_func(TestTask.timeout_handler)])
+        self.assertEqual(task.MessageReceived, types[self .get_func(TestTask.message_handler)])
+        self.assertEqual(task.Timeout, types[self.get_func(TestTask.timeout_handler)])
         self.assertEqual(task.Ended, types[self.get_func(TestTask.ended_handler)])
 
     def testInheritance(self):
+        """
+
+        :return:
+        """
         class BaseTask(task.Task):
+            """
+
+            """
             def __init__(self, *args, **kwargs):
+                """
+
+                :param args:
+                :param kwargs:
+                """
                 super(BaseTask, self).__init__(*args, **kwargs)
 
             @task.on_start()
             def start_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 pass
 
         class ConcreteTask(BaseTask):
+            """
+
+            """
             def __init__(self, *args, **kwargs):
+                """
+
+                :param args:
+                :param kwargs:
+                """
                 super(ConcreteTask, self).__init__(*args, **kwargs)
 
-            # overridden handler
+
             @task.on_start()
             def start_handler(self, event):
+                """ # overridden handler
+
+                :param event:
+                :return:
+                """
                 pass
 
         tt = ConcreteTask(max_time=10)
@@ -75,13 +138,36 @@ class TestEvents(unittest.TestCase):
         self.assertFalse(self.get_func(BaseTask.start_handler) in handlers)
 
     def testDynamicHandlers(self):
+        """
+
+        :return:
+        """
         class TestTask(task.Task):
+            """
+
+            """
             def __init__(self, *args, **kwargs):
+                """
+
+                :param args:
+                :param kwargs:
+                """
                 super(TestTask, self).__init__(*args, **kwargs)
 
             @task.on_start()
             def start_handler(self, event):
+                """
+
+                :param event:
+                :return:
+                """
                 def end_handler(self, event):
+                    """
+
+                    :param self:
+                    :param event:
+                    :return:
+                    """
                     pass
                 self.end_handler_func = end_handler
                 self.add_handler(task.on_ended()(end_handler))
@@ -90,14 +176,31 @@ class TestEvents(unittest.TestCase):
         tt = TestTask(max_time=10)
 
         class EnvironmentMock():
+            """
+
+            """
             def __init__(self, triggers):
+                """
+
+                :param triggers:
+                """
                 self.triggers = triggers
 
             def raise_event(self, event):
-                # we only generate an init event
+                """ we only generate an init event
+
+                :param event:
+                :return:
+                """
                 tt.start_handler(event)
 
             def _register_task_trigger(self, task, trigger):
+                """
+
+                :param task:
+                :param trigger:
+                :return:
+                """
                 self.triggers.append(trigger)
 
         # raise the start event
@@ -109,6 +212,11 @@ class TestEvents(unittest.TestCase):
         self.assertIn(self.get_func(tt.end_handler_func), handlers)
 
     def get_func(self, method):
+        """
+
+        :param method:
+        :return:
+        """
         try:
             return method.im_func
         except AttributeError: # Python 3
