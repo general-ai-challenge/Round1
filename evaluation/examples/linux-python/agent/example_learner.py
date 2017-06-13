98b9133ead1873c56d8891a97a270bb6e1aca202
Premek Paska <premysl.paska@goodai.com>
2017-06-13 16:28:10 +0200

    example_learner.py: allow to specify address and port via options
    - also allow to specify the port number

--- evaluation/examples/linux-python/agent/example_learner.py
+++ evaluation/examples/linux-python/agent/example_learner.py
@@ -1,17 +1,47 @@
 import zmq
 import signal
 import sys
+import getopt
 
+
+host = '172.18.0.1'
 port = 5556
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'a:p:', ['address=', 'port='])
+except getopt.GetoptError:
+    print("Error reading options. Usage:")
+    print("  example_learner.py [-a <address> -p <port>]")
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ('-a', '--address'):
+        host = arg
+    elif opt in ('-p', '--port'):
+        try:
+            port = int(arg)
+            if port < 1 or port > 65535:
+                raise ValueError("Out of range")
+        except ValueError:
+            print("Invalid port number: %s" % arg)
+            sys.exit(2)
+
 context = zmq.Context()
 socket = context.socket(zmq.PAIR)
-socket.connect("tcp://172.18.0.1:%s" % port)
+
+address = "tcp://%s:%s" % (host, port)
+print("Connecting to %s" % address)
+sys.stdout.flush()
+
+socket.connect(address)
 socket.send_string("hello")
+
 
 def handler(signal, frame):
     print('exiting...')
-    socket.disconnect("tcp://172.18.0.1:%s" % port)
+    socket.disconnect(address)
     exit()
+
 
 signal.signal(signal.SIGINT, handler)
 
@@ -19,7 +49,7 @@ reward = socket.recv()
 next_input = socket.recv()
 
 while True:
-    socket.send_string("a") # your attempt to solve the current task
+    socket.send_string("a")  # your attempt to solve the current task
     reward = socket.recv()
     next_input = socket.recv()
     print(reward)
